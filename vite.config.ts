d244609dd0bcd0f08221624fec4bd4ab2b7e15c9
ashoothakur92-prog <ashoothakur92@gmail.com>
2026-09-13 09:26:30 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base:'/urbanAtmo,
+    /base:'/urbanAtmo/,
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
